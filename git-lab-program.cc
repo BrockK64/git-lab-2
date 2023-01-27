941d5200a5ee4b1a8d5e4fce100c872339d4ac26
Brock Klamfoth <brock.klamfoth50@gmail.com>
2023-01-27 13:11:36 -0500

    E

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Brock
+ *        Date: 1/27/23
+ * Description: Program for Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
